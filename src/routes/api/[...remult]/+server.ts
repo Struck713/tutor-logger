93a93d9b14d662e37e5c59e8ae224c84ba2c377a
Struck713 <noahstruck71303@gmail.com>
2024-09-27 17:44:29 -0400

    Worked on lucia integration

--- src/routes/api/[...remult]/+server.ts
+++ src/routes/api/[...remult]/+server.ts
@@ -1,8 +1,15 @@
 import { remultSveltekit } from 'remult/remult-sveltekit'
 import { Request } from '../../../shared/Request';
+import { createKnexDataProvider } from "remult/remult-knex"
 
 export const _api = remultSveltekit({
-    entities: [Request]
+    entities: [Request],
+    dataProvider: createKnexDataProvider({
+        client: "sqlite3",
+        connection: {
+            filename: "./data.db"
+        }
+    })
 });
 
 export const { GET, POST, PUT, DELETE } = _api;
